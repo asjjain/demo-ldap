dba9f9804b25d1cdf3b612d632484e84e54e6626
ONELUX\u300444 <Ashish.JAIN@proximus.lu>
2023-01-10 15:02:47 +0100

    first commit

---
+++ src/main/java/lu/tango/demoldap/DemoLdapApplication.java
@@ -0,0 +1,13 @@
+package lu.tango.demoldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLdapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lu/tango/demoldap/DemoLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.tango.demoldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
